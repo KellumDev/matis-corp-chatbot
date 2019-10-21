690013f9828b7127b3452477d72a3a35d591cee3
Kellum <austin.kellum@accenture.com>
2019-10-21 15:46:13 -0500

    edit the input text box

--- rn-mywizard/src/components/chatbot/inputBox.js
+++ rn-mywizard/src/components/chatbot/inputBox.js
@@ -2,19 +2,18 @@ import React from 'react';
 
 const inputBox = (props) => {
     return (
-        <div></div>
+        <div class="row">
+            <div class="col s12 m6">
+                <div class="card indigo">
+                    <div class="card-content white-text">
 
-        // <div class="row">
-        //     <div class="col s12 m6">
-        //         <div class="card indigo">
-        //             <div class="card-content white-text">
+                        <input type="text" onKeyPress={props.keypress}  />
+                    </div>
 
-        //                 <input type="text" onKeyPress={props.keypress}  />
-        //             </div>
-
-        //         </div>
-        //     </div>
-        // </div>
+                </div>
+            </div>
+        </div>
+        
 
     );
 };
