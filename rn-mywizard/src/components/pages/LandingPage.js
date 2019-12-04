a49f9c519345b7cf7efc93772fee69e6c8376da5
Kellum <austin.kellum@accenture.com>
2019-12-04 12:44:46 -0600

    starting the accordion

--- rn-mywizard/src/components/pages/LandingPage.js
+++ rn-mywizard/src/components/pages/LandingPage.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import Chatbot from '../chatbot/chatbot'; 
+import Chatbot from '../chatbot/chatbot';
 //import Loader from '../loader/loader'; 
-import Logo from '../banner/banner'; 
+import Logo from '../banner/banner';
 
 const Landing = () => {
     return (
-        
+
         <div className="Landing-Page" style={styles.background} >
-            
-             <Logo />
+
+            <Logo />
+
             <Chatbot />
+             
         </div>
     )
-}  
+}
 const styles = {
     background: {
         margin: '5%'
